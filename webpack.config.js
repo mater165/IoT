5ce51e63688199fe14624eca24fa815bdc5dbbdb
Mattias Nielsen <mattiasnielsen@Mattias-MacBook-Pro.local>
2017-02-25 11:56:01 +0100

    config för webpack, lintar samt en index.html

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const buildConfig = function (env) {
+  const isProd = env === 'prod';
+  const folder = isProd ? './output/' : '/';
+  return {
+    entry: {
+      app: ['./testing/main']
+    },
+    output: {
+      filename: '[name].js',
+      path: folder
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: [
+            /node_modules/
+          ],
+          use: {loader: 'babel-loader', options: {presets: 'es2015'}}
+        }, {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+        }, {
+          test: /\.html$/,
+          include: path.resolve(__dirname, './src/html/index.html'),
+          use: 'html-loader'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js']
+    },
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        options: {
+          postcss: [require('autoprefixer')]
+        }
+      }),
+      new HtmlWebpackPlugin({
+        chunks: ['app'],
+        template: './src/html/index.html'
+      })
+    ],
+    stats: {
+      colors: true,
+      modules: true,
+      reasons: true,
+      errorDetails: true
+    }
+  };
+};
+module.exports = buildConfig;
